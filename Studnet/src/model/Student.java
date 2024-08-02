dabfc0f7e26289991283a9a6f925688ef3b7f445
HP <HP@LAPTOP-9A6JL2PJ>
2024-08-02 11:23:49 +0530

    new student project

---
+++ Studnet/src/model/Student.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Student {
+	
+	public void m1()
+	{
+		System.out.println("Hello");
+		
+			
+	}
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		
+	
+		
+
+	}
+
+}
